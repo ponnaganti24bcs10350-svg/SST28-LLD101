7b5fc79f1157836e914daffa567527f0c01f378e
ponnaganti24bcs10350-svg <ponnaganti.24bcs10350@sst.scaler.com>
2026-03-10 00:29:55 +0530

    Update SafeCashClient.java

--- adapter-payments/src/com/example/payments/SafeCashClient.java
+++ adapter-payments/src/com/example/payments/SafeCashClient.java
@@ -4,4 +4,7 @@ public class SafeCashClient {
     public SafeCashPayment createPayment(int amount, String user) {
         return new SafeCashPayment(amount, user);
     }
+    public String pay(String user, int amount) {
+        return "SC#" + user + ":" + amount;
+    }
 }
